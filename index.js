749a19cbff1100a5adb01f6f23d745a60580776b
PhamVanHoangAnh <anhpham761@gmail.com>
2021-05-14 09:55:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("Hello");
+
+console.log("Anh oi");
+
+var hello = "Xin chao";
+
+hello = "ni hao";
+console.log(hello);
+
+var giaTien = 1000;
+
+console.log(giaTien);
+
+var dungSai = true;
+dungSai = false;
+console.log(dungSai);
+
+var value1 = 2;
+var value2 = 6;
+var tong = value1+value2;
+var tich = value1*value2;
+var thuong = value1/value2;
+console.log("tong la:" , tong);
+console.log("hieu la :", value1-value2);
+console.log("tich la :" ,tich);
+console.log("thuong la :", thuong);
